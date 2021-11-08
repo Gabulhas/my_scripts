17b75bef0cca9745bd4d6f82253f7747dfb40e5b
Guilherme Lopes <Gabulhas@users.noreply.github.com>
2021-11-08 00:03:03 +0000

    updated meteo

--- random_wal.sh
+++ random_wal.sh
@@ -5,3 +5,4 @@ wallpapers_folder=/home/guilherme/.config/wallpapers
 random_image=`ls $wallpapers_folder| shuf -n 1`
 full_path="${wallpapers_folder}/${random_image}"
 wal -i $full_path
+wpg -s $full_path
--- wallpaper_menu.sh
+++ wallpaper_menu.sh
@@ -4,3 +4,4 @@ wallpapers_folder=/home/guilherme/.config/wallpapers
 random_image=`ls $wallpapers_folder| rofi -window-title Wallpaper -dmenu`
 full_path="${wallpapers_folder}/${random_image}"
 wal -i $full_path
+wpg -s $full_path
